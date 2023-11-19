486b9351826691c10a2738ad4b6814a3ad46d9c1
Ihor Yalosovetskyi <ihor.yalosovetskyi@programika.com>
2023-11-19 22:44:26 +0200

    readme

--- my_machine.h
+++ my_machine.h
@@ -29,6 +29,7 @@
 //#define BOARD_CITOH_CX6000    // C.ITOH CX-6000 HPGL plotter
 //#define BOARD_GENERIC_4AXIS
 //#define BOARD_MY_MACHINE      // Add my_machine_map.h before enabling this!
+#define BOARD_MY_MACHINE      // Add my_machine_map.h before enabling this!
 
 // Configuration
 // Uncomment to enable.
@@ -38,10 +39,11 @@
 #endif
 //#define SAFETY_DOOR_ENABLE   1 // Enable safety door input.
 //#define BLUETOOTH_ENABLE     2 // Set to 2 for HC-05 module, enable in CMakeLists.txt if for Pico W Bluetooth.
-//#define VFD_ENABLE           1 // Set to 1 or 2 for Huanyang VFD spindle. More here https://github.com/grblHAL/Plugins_spindle
-//#define MODBUS_ENABLE        7 // Set to 1 for auto direction, 2 for direction signal on auxillary output pin.
-//#define WIFI_ENABLE          0 // Do NOT enable here, enable in CMakeLists.txt!
-//#define WIFI_SOFTAP          1 // Use Soft AP mode for WiFi. NOTE: WIP - not yet complete!
+#define VFD_ENABLE             1 // Set to 1 works or 2 for Huanyang VFD spindle. More here https://github.com/grblHAL/Plugins_spindle
+#define MODBUS_ENABLE          1 // Set to 1 for auto direction, 2 for direction signal on auxillary output pin.
+#define MODBUS_BAUDRATE 2 // 9600
+#define WIFI_ENABLE            1 // Do NOT enable here, enable in CMakeLists.txt!
+#define WIFI_SOFTAP            1 // Use Soft AP mode for WiFi. NOTE: WIP - not yet complete!
 //#define ETHERNET_ENABLE      0 // Do NOT enable here, enable in CMakeLists.txt!
 //#define _WIZCHIP_         5500 // Selects WIZnet ethernet breakout connected via SPI.
                                  // Uncomment to enable W5500 chip, default is W5100S. Requires ethernet enabled in CMakeLists.txt.
@@ -89,32 +91,54 @@
 //#define MDNS_ENABLE          0 // mDNS daemon. Do NOT enable here, enable in CMakeLists.txt!
 //#define SSDP_ENABLE          1 // SSDP daemon - requires HTTP enabled.
 #if SDCARD_ENABLE || WEBUI_ENABLE
-#define FTP_ENABLE           1 // Ftp daemon - requires SD card enabled.
+//#define FTP_ENABLE           1 // Ftp daemon - requires SD card enabled.
 //#define HTTP_ENABLE          1 // http daemon - requires SD card enabled.
 //#define WEBDAV_ENABLE        1 // webdav protocol - requires http daemon and SD card enabled.
 #endif
 // The following symbols have the default values as shown, uncomment and change as needed.
-//#define NETWORK_STA_HOSTNAME    "grblHAL"
-//#define NETWORK_STA_IPMODE      1 // 0 = static, 1 = DHCP, 2 = AutoIP
-//#define NETWORK_STA_IP          "192.168.5.1"
-//#define NETWORK_STA_GATEWAY     "192.168.5.1"
-//#define NETWORK_STA_MASK        "255.255.255.0"
+#define NETWORK_STA_HOSTNAME    "grblHAL"
+#define NETWORK_STA_IPMODE      1 // 0 = static, 1 = DHCP, 2 = AutoIP
+#define COMPATIBILITY_LEVEL 2
+
+#define NETWORK_HOSTNAME        "grbl"
+#define NETWORK_IPMODE          1 // 0 = static, 1 = DHCP, 2 = AutoIP
+#define NETWORK_IP              "192.168.4.1"
+#define NETWORK_GATEWAY         "192.168.4.1"
+#define NETWORK_PASSWORD     "grbl"
+#define NETWORK_MASK            "255.255.255.0"
+
+#define NETWORK_STA_SSID         "IGORNET"
+#define NETWORK_STA_PASSWORD     "IG0RNET29041971" 
+
+#define NETWORK_STA_IP          "10.80.39.130"
+#define NETWORK_STA_GATEWAY     "10.80.39.3"
+#define NETWORK_STA_MASK        "255.255.255.0"
+
 #if WIFI_SOFTAP > 0
-//#define NETWORK_AP_SSID         "grblHAL_AP"
-//#define NETWORK_AP_PASSWORD     "grblHALap"
-//#define NETWORK_AP_HOSTNAME     "grblHAL_AP"
-//#define NETWORK_AP_IPMODE       0              // Do not change!
-//#define NETWORK_AP_IP           "192.168.4.1"  // Do not change!
-//#define NETWORK_AP_GATEWAY      "192.168.4.1"  // Do not change!
-//#define NETWORK_AP_MASK         "255.255.255.0"
+#define NETWORK_AP_SSID         "grblHAL_AP"
+#define NETWORK_AP_PASSWORD     "grbl"
+#define NETWORK_AP_HOSTNAME     "grbl"
+#define NETWORK_AP_IPMODE       1              // Do not change!
+#define NETWORK_AP_IP           "192.168.4.1"  // Do not change!
+#define NETWORK_AP_GATEWAY      "192.168.4.1"  // Do not change!
+#define NETWORK_AP_MASK         "255.255.255.0"
+#define WIFI_STA_SSID "IGORNET"
+#define WIFI_STA_PASSWORD "IG0RNET29041971" // Minimum 8 characters, or blank for open
+
+#else
+#define WIFI_SOFTAP 0
+#define WIFI_MODE WiFiMode_STA; // Do not change!
+#define WIFI_STA_SSID "IGORNET"
+#define WIFI_STA_PASSWORD "IG0RNET29041971" // Minimum 8 characters, or blank for open
 #endif
+
 //#define NETWORK_FTP_PORT     21
 //#define NETWORK_TELNET_PORT  23
-//#define NETWORK_HTTP_PORT    80
+#define NETWORK_HTTP_PORT    80
 #if HTTP_ENABLE
-//#define NETWORK_WEBSOCKET_PORT  81
+#define NETWORK_WEBSOCKET_PORT  81
 #else
-//#define NETWORK_WEBSOCKET_PORT  80
+#define NETWORK_WEBSOCKET_PORT  80
 #endif
 #endif // WIFI_ENABLE
 
